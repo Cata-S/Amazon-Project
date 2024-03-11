afcda7fb04512fe1c418d8019b5d16e9b055f2fd
Cata <c.silav@yahoo.com>
2024-03-11 17:26:51 +0200

    moved the update/delete buttons from checkout.js to cart.js and exported it to checkout,js

--- data/cart.js
+++ data/cart.js
@@ -1,3 +1,4 @@
+// cart.js
 export const cart = [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2,
@@ -21,3 +22,66 @@ export function addToCart(cart, productId, quantity) {
     cart.push({productId: productId, quantity: quantity});
   }
 };
+
+export function initializeButtons() {
+  let deleteButtons = document.querySelectorAll('.js-delete-link');
+  let updateButtons = document.querySelectorAll('.update-quantity-link');
+
+  deleteButtons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      let quantityLabel = button.parentElement.querySelector('.quantity-label');
+      let quantity = parseInt(quantityLabel.textContent);
+
+      if (quantity > 1) {
+        quantityLabel.textContent = quantity - 1;
+      }
+      else if (quantity === 1) {
+        button.closest('.cart-item-container').remove();
+      }
+    });
+  });
+
+  updateButtons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      let select = document.createElement('select');
+      for (let i = 1; i <= 9; i++) {
+        let option = document.createElement('option');
+        option.value = i;
+        option.text = i;
+        select.appendChild(option);
+      }
+      let option = document.createElement('option');
+      option.value = '10+';
+      option.text = '10+';
+      select.appendChild(option);
+      button.replaceWith(select);
+
+      select.addEventListener('change', (event) => {
+        let quantityLabel = select.parentElement.querySelector('.quantity-label');
+        if (event.target.value === '10+') {
+          let input = document.createElement('input');
+          input.type = 'number';
+          input.min = '1';
+          input.size = '2';
+          input.maxLength = '2';
+          select.replaceWith(input);
+
+          input.addEventListener('blur', (event) => {
+            quantityLabel.textContent = event.target.value;
+            input.replaceWith(button);
+          });
+        } else {
+          quantityLabel.textContent = event.target.value;
+          select.replaceWith(button);
+        }
+      });
+    });
+  });
+}
+
+
+
+
+
+
+
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, initializeButtons} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatMoney } from '../utils/money.js';
 
@@ -96,68 +96,7 @@ cartSummaryHTML += `
 });
 
 document.querySelector('.js-cart-summary').innerHTML = cartSummaryHTML;
+initializeButtons();
 
-let deleteButtons = document.querySelectorAll('.js-delete-link');
 
-deleteButtons.forEach((button) => {
-  button.addEventListener('click', (event) => {
-    
-    let quantityLabel = button.parentElement.querySelector('.quantity-label');
-    let quantity = parseInt(quantityLabel.textContent);
 
-    if (quantity > 1) {
-      quantityLabel.textContent = quantity - 1;
-    }
-    else if (quantity === 1) {
-      // Traverse up the DOM tree to the closest 'product' div and remove that
-      button.closest('.cart-item-container').remove();
-    }
-  });
-});
-
-// Update button
-let updateButtons = document.querySelectorAll('.update-quantity-link');
-
-updateButtons.forEach((button) => {
-  button.addEventListener('click', (event) => {
-    let select = document.createElement('select');
-    for (let i = 1; i <= 9; i++) {
-      let option = document.createElement('option');
-      option.value = i;
-      option.text = i;
-      select.appendChild(option);
-    }
-    let option = document.createElement('option');
-    option.value = '10+';
-    option.text = '10+';
-    select.appendChild(option);
-    button.replaceWith(select);
-
-    select.addEventListener('change', (event) => {
-      let quantityLabel = select.parentElement.querySelector('.quantity-label');
-      if (event.target.value === '10+') {
-        let input = document.createElement('input');
-        input.type = 'number';
-        input.min = '1';
-        input.size = '2'; 
-        input.maxLength = '2'; 
-
-        input.style.width = '50px'; 
-        input.style.height = '20px';
-
-        select.replaceWith(input);
-
-        input.addEventListener('keydown', (event) => {
-          if (event.key === 'Enter') { 
-            event.preventDefault();
-            quantityLabel.textContent = event.target.value;
-            input.replaceWith(button);
-          }
-        });
-      } else {
-        quantityLabel.textContent = event.target.value;
-        select.replaceWith(button);
-      }
-    });
-  });
-});
